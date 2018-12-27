b0805ae0c79558083fe5a790b4d9a72b1a03fe36
minhpham <minh.pham@dhigroupinc.com>
2018-12-27 15:06:55 -0600

    handle error

--- hello-consumer-microservice-message/src/main/java/io/vertx/book/message/HelloConsumerMicroservice.java
+++ hello-consumer-microservice-message/src/main/java/io/vertx/book/message/HelloConsumerMicroservice.java
@@ -2,9 +2,12 @@ package io.vertx.book.message;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.RxHelper;
 import io.vertx.rxjava.core.eventbus.EventBus;
 import io.vertx.rxjava.core.eventbus.Message;
 import rx.Single;
+
+import java.util.concurrent.TimeUnit;
 
 public class HelloConsumerMicroservice extends AbstractVerticle {
 
@@ -12,20 +15,19 @@ public class HelloConsumerMicroservice extends AbstractVerticle {
     public void start() {
         vertx.createHttpServer().requestHandler(req -> {
             EventBus bus = vertx.eventBus();
-            Single<JsonObject> lukeSingle = bus.<JsonObject>rxSend("hello", "Luke").map(Message::body);
-            Single<JsonObject> leiaSingle = bus.<JsonObject>rxSend("hello", "Leia").map(Message::body);
+            Single<JsonObject> lukeSingle = bus.<JsonObject>rxSend("hello", "Luke")
+                    .subscribeOn(RxHelper.scheduler(vertx)).timeout(3, TimeUnit.SECONDS).retry(3).map(Message::body);
+            Single<JsonObject> leiaSingle = bus.<JsonObject>rxSend("hello", "Leia")
+                    .subscribeOn(RxHelper.scheduler(vertx)).timeout(3, TimeUnit.SECONDS).retry(3).map(Message::body);
             Single.zip(lukeSingle, leiaSingle, (luke, leia) -> new JsonObject()
                     .put("Luke", luke.getString("message") + " from "
                             + luke.getString("served_by")).put("Leia", leia.getString("message") + " from "
                             + leia.getString("served_by")))
                     .subscribe(result -> req.response().setStatusCode(200).end(result.encode()), error -> {
                         error.printStackTrace();
-                        req.response().setStatusCode(500).end();
-                        error.getMessage();
+                        req.response().setStatusCode(500).end(error.getMessage());
                     });
         }).rxListen(8080).subscribe(result -> System.out.println("done"), Throwable::printStackTrace);
-
-
     }
 
 }
--- hello-microservice-message/src/main/java/io/vertx/book/message/HelloMicroservice.java
+++ hello-microservice-message/src/main/java/io/vertx/book/message/HelloMicroservice.java
@@ -8,11 +8,19 @@ public class HelloMicroservice extends AbstractVerticle {
     @Override
     public void start() {
         vertx.eventBus().<String>consumer("hello", stringMessage -> {
-            JsonObject jsonObject = new JsonObject().put("served_by", this.toString());
-            if (stringMessage.body().isEmpty()) {
-                stringMessage.reply(jsonObject.put("message", "hello"));
-            } else {
-                stringMessage.reply(jsonObject.put("message", "hello " + stringMessage.body()));
+            double chaos = Math.random();
+            if(chaos <0.3) {
+                JsonObject jsonObject = new JsonObject().put("served_by", this.toString());
+                if (stringMessage.body().isEmpty()) {
+                    stringMessage.reply(jsonObject.put("message", "hello"));
+                } else {
+                    stringMessage.reply(jsonObject.put("message", "hello " + stringMessage.body()));
+                }
+            }else if(chaos < .9){
+                System.out.println("Returning error");
+                stringMessage.fail(500,"message processing failed");
+            }else{
+                System.out.println("Will not return reply");
             }
         });
     }
